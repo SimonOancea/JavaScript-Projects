899919bec5107e946ef62273f3f7011cbebe85e5
SimonOancea <159942913+SimonOancea@users.noreply.github.com>
2024-03-28 16:58:41 +0000

    Added a web using JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello world!") //This is an alert
+document.write(" This site is styled using JavaScript"); //this is a text
+var Sent1="This is the beginning of the string", Sent2="and this is the end of the string"; //2 variables through one statement
+document.write(Sent1+Sent2); //2 variables combine and display
+document.write(5+5); //an expression
