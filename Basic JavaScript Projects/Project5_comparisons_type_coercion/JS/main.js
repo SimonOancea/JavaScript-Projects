e5ad94efdc37b87921b326348412efc751d88c3e
SimonOancea <159942913+SimonOancea@users.noreply.github.com>
2024-04-03 17:33:15 +0100

    Added comparasions

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,17 @@
+var x= true;
+document.write("<br>");
+document.write(typeof x);
+document.write("<br>");
+document.write(7+"3");
+document.write("<br>");
+document.write(1==1); // 1 is equal with 1 - true
+document.write("<br>");
+y=2;x=2;
+document.write(y==x); // y is equal with x - true
+document.write("<br>");
+document.write(1==="car"); // 1 is equal with 'car' - false
+document.write(1==1 && 2==2); // 1 is equal with 1 and 2 is equal with 2 - true
+document.write("<br>");
+document.write(1==2 || 1==3); // 1 is equal with 2 or 3 - false
+document.write("<br>");
+document.write(3!=4); // 3 is NOT equal with 4 - true
